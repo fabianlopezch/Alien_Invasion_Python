cf5baae0feee344800dd651627a691f85d1d52a5
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-02-03 17:21:02 -0500

    Refactoring: the _check_events() method

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,10 +27,8 @@ class AlienInvasion:
 		""" Start the main loop for the game. """
 
 		while True:
-			# Watch for keyboard and mouse events.
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
+
+			self._check_events()
 
 			# Redraw the screen during each pass through the loop.
 			self.screen.fill(self.settings.bg_color)
@@ -39,6 +37,13 @@ class AlienInvasion:
 
 			# Make the most recently drawn screen visible. 
 			pygame.display.flip()
+
+	def _check_events(self): # Helper method to isolate the event manager loop
+		""" Respond to keypresses and mouse events. """
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
 
 if __name__ == '__main__':
 	# Make a game instance, and run the game.
