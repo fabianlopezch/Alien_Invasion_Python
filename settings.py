2dddad28ff424fdf6f0fbbfeb987dad7f1371bae
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-02-05 23:40:50 -0500

    Adding the bullet settings

--- settings.py
+++ settings.py
@@ -11,3 +11,11 @@ class Settings:
 
 		# Ship settings
 		self. ship_speed = 1.5
+
+		# Bullet settings
+		self.bullet_speed = 1.0
+		self.bullet_width = 3 # Number of pixels
+		self.bullet_height = 15 # Number of pixels
+		self.bullet_color = (60, 60, 60)
+
+		
