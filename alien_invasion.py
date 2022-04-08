c551e2e4aa2007b681ddb90ae3ac687625e605bd
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-04-07 20:01:25 -0500

    Starting the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -69,6 +69,10 @@ class AlienInvasion:
 			elif event.type == pygame.KEYUP:
 				self._check_keyup_events(event)
 
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				mouse_pos = pygame.mouse.get_pos()
+				self._check_play_button(mouse_pos)
+
 
 	def _check_keydown_events(self, event):
 		""" Respond to keypresses. """
@@ -227,6 +231,11 @@ class AlienInvasion:
 
 		# Make the most recently drawn screen visible. 
 		pygame.display.flip()
+
+	def _check_play_button(self, mouse_pos):
+		"""Start a new game when the player clicks Play."""
+		if self.play_button.rect.collidepoint(mouse_pos):
+			self.stats.game_active = True
 
 
 if __name__ == '__main__':
