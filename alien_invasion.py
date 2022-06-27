191e4ae58bcbf8d5b84cb75ef14e9706a593fe60
Fabian Lopez Chamorro <fabianlopezch@outlook.com>
2022-06-27 01:51:37 -0500

    Displaying the level

--- alien_invasion.py
+++ alien_invasion.py
@@ -134,6 +134,10 @@ class AlienInvasion:
 			self._create_fleet()
 			self.settings.increase_speed()
 
+			# Increase level.
+			self.stats.level += 1
+			self.sb.prep_level()
+
 		if collisions:
 			for aliens in collisions.values():
 				self.stats.score += self.settings.alien_points * len(aliens) 
@@ -256,6 +260,7 @@ class AlienInvasion:
 			self.stats.reset_stats()
 			self.stats.game_active = True
 			self.sb.prep_score()
+			self.sb.prep_level()
 
 			# # Get rid of any remaining aliens and bullets.
 			self.aliens.empty()
